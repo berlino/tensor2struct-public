8dde1665046b32a16b58f89bbcfcff1281d0fc58
Bailin <bailin.wang28@gmail.com>
2022-01-20 23:18:58 +0000

    Update __init__.py
    import modules

--- tensor2struct/models/__init__.py
+++ tensor2struct/models/__init__.py
@@ -1,7 +1,15 @@
 from . import arithmetic
+from . import cogs
+from . import spider
+from . import overnight
+from . import scan
 
+from . import ast_decoder
 from . import enc_dec
 from . import beam_search
-
+from . import sampling
+from . import batched_encoder
+from . import batched_decoder
+from . import batched_beam_search
 from . import latper_encoder
 from . import tagging_decoder
