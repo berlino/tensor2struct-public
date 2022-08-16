eea09e82480d8cd5d1e7f5b6fc5b99b2325e5ae1
yscope <thienyglobe@gmail.com>
2022-08-16 21:34:16 +0700

    fix infered json dump to file

--- tensor2struct/commands/infer.py
+++ tensor2struct/commands/infer.py
@@ -150,7 +150,7 @@ class Inferer:
                             "beams": decoded
                         },
                         ensure_ascii=False
-                    ).encode('utf8'))
+                    ))
                 output.write("\n")
             output.flush()
 
